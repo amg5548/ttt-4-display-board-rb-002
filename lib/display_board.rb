57843902b66633730c41ad949756670a32fba21a
amg5548 <amg5548@gmail.com>
2016-01-30 23:51:08 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board (board)
+  symbol = ["|", "-----------"]
+  board = ["   ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "]
+
+  puts "#{board[0]}#{symbol [0]}#{board[1]}#{symbol [0]}#{board[2]}"
+  puts "#{symbol[1]}"
+  puts "#{board[3]}#{symbol [0]}#{board[4]}#{symbol [0]}#{board[5]}"
+  puts "#{symbol[1]}"
+  puts "#{board[6]}#{symbol [0]}#{board[7]}#{symbol [0]}#{board[8]}"
+end
