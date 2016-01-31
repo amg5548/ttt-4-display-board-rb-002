72972271fd82cbfe866c9562921ac8f3aa0b03ca
amg5548 <amg5548@gmail.com>
2016-01-31 00:33:01 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,74 @@
 def display_board (board)
   symbol = ["|", "-----------"]
-  board = ["   ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "]
+  board = [" X ", " X ", " X ", "   ", " X ", "   ", "   ", "   ", "   "]
+  board2 = [" O ", "   ", "   ", "   ", "   ", "   ", " O ", " O ", " O "]
+  board3 = [" X ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", " X "]
+  board4 = ["   ", "   ", " O ", "   ", " O ", "   ", " O ", "   ", " O "]
+  board5 = [" O ", " X ", " X ", " X ", " O ", " O ", " X ", " O ", " X "]
+  board6 = [" O ", " X ", " O ", " X ", " X ", " X ", "   ", " X ", "   "]
+  board7 = ["   ", "   ", "   ", " O ", " O ", " O ", " X ", "   ", "   "]
+  board8 = ["   ", " X ", "   ", "   ", "   ", " X ", "   ", "   ", " O "]
+  board9 = ["   ", " O ", "   ", " O ", "   ", " O ", " O ", " X ", " X "]
+  board10 = [" X ", " O ", " O ", " X ", " O ", " X ", " O ", " X ", " O "]
 
   puts "#{board[0]}#{symbol [0]}#{board[1]}#{symbol [0]}#{board[2]}"
   puts "#{symbol[1]}"
   puts "#{board[3]}#{symbol [0]}#{board[4]}#{symbol [0]}#{board[5]}"
   puts "#{symbol[1]}"
   puts "#{board[6]}#{symbol [0]}#{board[7]}#{symbol [0]}#{board[8]}"
+
+  puts "#{board2[0]}#{symbol [0]}#{board2[1]}#{symbol [0]}#{board2[2]}"
+  puts "#{symbol[1]}"
+  puts "#{board2[3]}#{symbol [0]}#{board2[4]}#{symbol [0]}#{board2[5]}"
+  puts "#{symbol[1]}"
+  puts "#{board2[6]}#{symbol [0]}#{board2[7]}#{symbol [0]}#{board2[8]}"
+
+  puts "#{board3[0]}#{symbol [0]}#{board3[1]}#{symbol [0]}#{board3[2]}"
+  puts "#{symbol[1]}"
+  puts "#{board3[3]}#{symbol [0]}#{board3[4]}#{symbol [0]}#{board3[5]}"
+  puts "#{symbol[1]}"
+  puts "#{board3[6]}#{symbol [0]}#{board3[7]}#{symbol [0]}#{board3[8]}"
+
+  puts "#{board4[0]}#{symbol [0]}#{board4[1]}#{symbol [0]}#{board4[2]}"
+  puts "#{symbol[1]}"
+  puts "#{board4[3]}#{symbol [0]}#{board4[4]}#{symbol [0]}#{board4[5]}"
+  puts "#{symbol[1]}"
+  puts "#{board4[6]}#{symbol [0]}#{board4[7]}#{symbol [0]}#{board4[8]}"
+
+  puts "#{board5[0]}#{symbol [0]}#{board5[1]}#{symbol [0]}#{board5[2]}"
+  puts "#{symbol[1]}"
+  puts "#{board5[3]}#{symbol [0]}#{board5[4]}#{symbol [0]}#{board5[5]}"
+  puts "#{symbol[1]}"
+  puts "#{board5[6]}#{symbol [0]}#{board5[7]}#{symbol [0]}#{board5[8]}"
+
+  puts "#{board6[0]}#{symbol [0]}#{board6[1]}#{symbol [0]}#{board6[2]}"
+  puts "#{symbol[1]}"
+  puts "#{board6[3]}#{symbol [0]}#{board6[4]}#{symbol [0]}#{board6[5]}"
+  puts "#{symbol[1]}"
+  puts "#{board6[6]}#{symbol [0]}#{board6[7]}#{symbol [0]}#{board6[8]}"
+
+  puts "#{board7[0]}#{symbol [0]}#{board7[1]}#{symbol [0]}#{board7[2]}"
+  puts "#{symbol[1]}"
+  puts "#{board7[3]}#{symbol [0]}#{board7[4]}#{symbol [0]}#{board7[5]}"
+  puts "#{symbol[1]}"
+  puts "#{board7[6]}#{symbol [0]}#{board7[7]}#{symbol [0]}#{board7[8]}"
+
+  puts "#{board8[0]}#{symbol [0]}#{board8[1]}#{symbol [0]}#{board8[2]}"
+  puts "#{symbol[1]}"
+  puts "#{board8[3]}#{symbol [0]}#{board8[4]}#{symbol [0]}#{board8[5]}"
+  puts "#{symbol[1]}"
+  puts "#{board8[6]}#{symbol [0]}#{board8[7]}#{symbol [0]}#{board8[8]}"
+
+  puts "#{board9[0]}#{symbol [0]}#{board9[1]}#{symbol [0]}#{board9[2]}"
+  puts "#{symbol[1]}"
+  puts "#{board9[3]}#{symbol [0]}#{board9[4]}#{symbol [0]}#{board9[5]}"
+  puts "#{symbol[1]}"
+  puts "#{board9[6]}#{symbol [0]}#{board9[7]}#{symbol [0]}#{board9[8]}"
+
+  puts "#{board10[0]}#{symbol [0]}#{board10[1]}#{symbol [0]}#{board10[2]}"
+  puts "#{symbol[1]}"
+  puts "#{board10[3]}#{symbol [0]}#{board10[4]}#{symbol [0]}#{board10[5]}"
+  puts "#{symbol[1]}"
+  puts "#{board10[6]}#{symbol [0]}#{board10[7]}#{symbol [0]}#{board10[8]}"
+
 end
